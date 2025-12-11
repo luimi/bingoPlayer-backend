f4d215eac927019405e912502c6cf459197702d7
Luis Meza <luimi@live.com>
2025-12-11 10:36:37 -0500

    ajuste de prompt

--- utils/constants.js
+++ utils/constants.js
@@ -31,4 +31,4 @@ Si no se encuentran cartones de bingo legibles en la imagen, devuelve un array J
 
 No incluyas texto explicativo, solo el JSON.`;
 
-export const prompt = `Analiza esta imagen y devuelve un array en JSON donde extraigas los numeros de bingo en este formato [[[1,15,23,43,69], [2,16,24,45,70], ...]] donde el primer arreglo contiene cada carton de bingo que veas, si en alguno de las celdas no hay numero entero, rellena los campos con el numero cero, si no hay cartones legibles devuelve un array vacio`;
+export const prompt = `Analiza esta imagen y devuelve un array en JSON donde extraigas los numeros de bingo en este formato [[[1,15,23,43,69], [2,16,24,45,70], ...]] donde el primer arreglo contiene cada carton de bingo que veas, si en alguno de las celdas no hay numero entero o esta en blanco, rellena los campos con el numero cero, si no hay cartones legibles devuelve un array vacio`;
